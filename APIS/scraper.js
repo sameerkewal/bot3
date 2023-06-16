5f1185fd57814ead2c0427a1de629c6180fc97e2
sameerkewal <sameerkewal1@gmail.com>
2023-06-15 22:06:57 -0300

    scraper

--- APIS/scraper.js
+++ APIS/scraper.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 async function getResult(){
     console.log(process.env.LINK)
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless:"new"});
 
     const page = await browser.newPage();
 
